02bc660e9d475435aa47e0c96de0a1372b49f2a7
Hemant <hemant.phutela@gmail.com>
2019-02-25 23:53:38 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books308"
+  //Remote MongoDB deployment -> let 
+  "URI" :"mongodb://kajal:kajal123@cluster0-shard-00-00-qkyf8.mongodb.net:27017,cluster0-shard-00-01-qkyf8.mongodb.net:27017,cluster0-shard-00-02-qkyf8.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+
+  //mongodb://kajal:<PASSWORD>@cluster0-shard-00-00-qkyf8.mongodb.net:27017,cluster0-shard-00-01-qkyf8.mongodb.net:27017,cluster0-shard-00-02-qkyf8.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
+};
